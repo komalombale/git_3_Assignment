015feb9de5443989b39277202922e183cb3a5f09
komalombale <ombalekomal6@gmail.com>
2022-11-02 16:46:18 +0530

    prime code

---
+++ prime_code.js
@@ -0,0 +1,15 @@
+
+function checkprime(number){
+	
+    	for(let i=1;i<=n;i++){
+        	if(number%i==0){
+            count++;
+        	
+    	}
+ if (count ==2){
+        	return true;
+    	}
+        	return false;
+    	}
+
+
